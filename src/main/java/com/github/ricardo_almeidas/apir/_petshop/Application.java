038480ab9ba2d05e6a1b197d73ec43859c927fe2
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-16 20:34:01 -0300

    feat: apir petshop finalizado

---
+++ src/main/java/com/github/ricardo_almeidas/apir/_petshop/Application.java
@@ -0,0 +1,13 @@
+package com.github.ricardo_almeidas.apir._petshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
